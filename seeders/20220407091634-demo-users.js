5abbe97555a582c8af3875071caa4d06c85471f0
Sherif Tarek <sherift1552@gmail.com>
2022-04-07 11:25:07 +0200

    Add User Model, Migration, and Seeder

---
+++ seeders/20220407091634-demo-users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [
+      {
+        name: 'John Doe',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, 
+      {
+        name: 'Sherif Tarek',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Mahmoud Sorour',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Iain Bowes',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
